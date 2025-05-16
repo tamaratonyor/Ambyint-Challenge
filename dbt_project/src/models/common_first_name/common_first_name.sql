aae2c54307da0ab07c0bc1b65bddcb709946726b
Tamaratonyor Ebufegha <tammyebufegha@Tamaratonyors-MacBook-Air.local>
2025-05-16 13:38:39 -0600

    add common first name model

---
+++ dbt_project/src/models/common_first_name/common_first_name.sql
@@ -0,0 +1,20 @@
+WITH flattened_casts AS (
+    SELECT 
+        TRIM(SPLIT_PART(actor.value::STRING, ' ', 1)) AS first_name
+    FROM {{ ref('raw_merged') }} AS r
+    , LATERAL FLATTEN(input => SPLIT(r.cast, ',')) AS actor
+),
+
+filtered_names AS (
+    SELECT *
+    FROM flattened_casts
+    WHERE first_name IS NOT NULL AND first_name <> ''
+)
+
+SELECT 
+    first_name,
+    COUNT(*) AS appearances
+FROM filtered_names
+GROUP BY first_name
+ORDER BY appearances DESC
+LIMIT 1;
--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 python script.py
+dbt deps
 dbt seed --full-refresh && dbt run
